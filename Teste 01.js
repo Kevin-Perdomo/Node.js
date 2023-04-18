a9708784f47cb275b39a077d62e74a7eb55a0756
Kevin-Perdomo <119632064+Kevin-Perdomo@users.noreply.github.com>
2023-04-17 22:35:52 -0300

    Learning Node.js

---
+++ Teste 01.js
@@ -0,0 +1,21 @@
+console.log("\n\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\n");
+
+//print
+console.log("Hello world!");
+
+//vetores
+var nome = "Kevin";
+var sobrenome = "Siqueira Perdomo";
+console.log("\n\n" + nome + " " + sobrenome + "\n\n");
+
+//soma com função
+var n1 = 15;
+var n2 = 5;
+
+function somar(a,b){
+    return a + b;
+}
+
+console.log(somar(n1,n2));
+
+console.log("\n\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\n");
