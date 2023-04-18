fe97601f45a29a58c9c82264ee4b8cd36554c1b9
Kevin-Perdomo <119632064+Kevin-Perdomo@users.noreply.github.com>
2023-04-17 22:39:09 -0300

    Learning Node.js

--- Calculadora/Calculadora.js
+++ Calculadora/Calculadora.js
@@ -3,7 +3,6 @@ var subtrai_function = require("./função subtrair");
 var multiplica_function = require("./função multiplicar");
 var divide_function = require("./função dividir");
 
-
 console.log(soma_function(10,15));
 console.log(subtrai_function(10,15));
 console.log(multiplica_function(10,15));
